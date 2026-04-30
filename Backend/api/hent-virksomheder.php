3e8bc44afac757c35b28649b6bbe7b37f0ba9607
Charlotte <s214408@dtu.dk>
2026-04-30 14:09:10 +0200

    tried to add so i can read pdf files too

--- Backend/api/hent-virksomheder.php
+++ Backend/api/hent-virksomheder.php
@@ -45,93 +45,109 @@ function hentRegnskabsData($cvr) {
     
     $response = curl_exec($ch);
     curl_close($ch);
+
     
     if (!$response) return null;
     
     $data = json_decode($response, true);
-    
-    // Find XBRL URL
-    $xbxlUrl = null;
+
+    $dokumentUrl = null;
+    $dokumentType = null;
+
     if (isset($data['hits']['hits'][0]['_source']['dokumenter'])) {
         $dokumenter = $data['hits']['hits'][0]['_source']['dokumenter'];
         for ($i = count($dokumenter) - 1; $i >= 0; $i--) {
             if ($dokumenter[$i]['dokumentMimeType'] == 'application/xml') {
-                $xbxlUrl = $dokumenter[$i]['dokumentUrl'];
+                $dokumentUrl = $dokumenter[$i]['dokumentUrl'];
+                $dokumentType = 'xml';
                 break;
             }
         }
+    //hvis ingen xml fundet
+    if(!$dokumentUrl) {
+        for ($i = count($dokumenter) - 1; $i >= 0; $i--) {
+            if ($dokumenter[$i]['dokumentMimeType'] == 'application/pdf') {
+                $dokumentUrl = $dokumenter[$i]['dokumentUrl'];
+                $dokumentType = 'pdf';
+                break;
+                }
+            }
+        }
     }
-    
-    if (!$xbxlUrl) return null;
-    
-    $ch = curl_init($xbxlUrl);
+    if(!$dokumentUrl) {
+        return null;
+    }
+
+    $ch = curl_init($dokumentUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36');
     curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
     curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     
-    $xml = curl_exec($ch);
+    $content = curl_exec($ch);
     curl_close($ch);
-    
-    if (!$xml) return null;
+
+    if(!$content) return null;
     
     $nogletal = [];
     
     // Omsætning (Revenue)
-    if (preg_match('/<[a-z]+:Revenue[^>]*>(\d+)/i', $xml, $match)) {                 
+    if (preg_match('/<[a-z]+:Revenue[^>]*>(\d+)/i', $content, $match)) {                 
         $nogletal['omsaetning'] = (int)$match[1];
-    } elseif (preg_match('/Revenue[^>]*>(\d+)/i', $xml, $match)) {
+    } elseif (preg_match('/Revenue[^>]*>(\d+)/i', $content, $match)) {
+        $nogletal['omsaetning'] = (int)$match[1];
+    } elseif (preg_match('/<[a-z]+:GrossProfitLoss[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['omsaetning'] = (int)$match[1];
     }
 
     // Resultat (ProfitLoss)
-    if (preg_match('/<[a-z]+:ProfitLoss[^>]*>(\d+)/i', $xml, $match)) {
+    if (preg_match('/<[a-z]+:ProfitLoss[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['resultat'] = (int)$match[1];
-    } elseif (preg_match('/<[a-z]+:NetProfitLoss[^>]*>(\d+)/i', $xml, $match)) {
+    } elseif (preg_match('/<[a-z]+:NetProfitLoss[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['resultat'] = (int)$match[1];
-    } elseif (preg_match('/ProfitLoss[^>]*>(\d+)/i', $xml, $match)) {
+    } elseif (preg_match('/ProfitLoss[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['resultat'] = (int)$match[1];
     }
 
     // Egenkapital (Equity)
-    if (preg_match('/<[a-z]+:Equity[^>]*>(\d+)/i', $xml, $match)) {
+    if (preg_match('/<[a-z]+:Equity[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['egenkapital'] = (int)$match[1];
-    } elseif (preg_match('/<[a-z]+:EquityAttributableToOwnersOfParent[^>]*>(\d+)/i', $xml, $match)) {
+    } elseif (preg_match('/<[a-z]+:EquityAttributableToOwnersOfParent[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['egenkapital'] = (int)$match[1];
-    } elseif (preg_match('/Equity[^>]*>(\d+)/i', $xml, $match)) {
+    } elseif (preg_match('/Equity[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['egenkapital'] = (int)$match[1];
     }
 
     // Aktiver (Assets)
-    if (preg_match('/<[a-z]+:Assets[^>]*>(\d+)/i', $xml, $match)) {
+    if (preg_match('/<[a-z]+:Assets[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['aktiver'] = (int)$match[1];
-    } elseif (preg_match('/Assets[^>]*>(\d+)/i', $xml, $match)) {
+    } elseif (preg_match('/Assets[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['aktiver'] = (int)$match[1];
     }
 
     // Omsætningsaktiver (CurrentAssets)
-    if (preg_match('/<[a-z]+:CurrentAssets[^>]*>(\d+)/i', $xml, $match)) {
+    if (preg_match('/<[a-z]+:CurrentAssets[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['omsaetningsaktiver'] = (int)$match[1];
-    } elseif (preg_match('/<[a-z]+:TotalCurrentAssets[^>]*>(\d+)/i', $xml, $match)) {
+    } elseif (preg_match('/<[a-z]+:TotalCurrentAssets[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['omsaetningsaktiver'] = (int)$match[1];
-    } elseif (preg_match('/CurrentAssets[^>]*>(\d+)/i', $xml, $match)) {
+    } elseif (preg_match('/CurrentAssets[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['omsaetningsaktiver'] = (int)$match[1];
     } 
-    elseif (preg_match('/Current\s+assets[^>]*>(\d+)/i', $xml, $match)) {
+    elseif (preg_match('/Current\s+assets[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['omsaetningsaktiver'] = (int)$match[1];
     }
 
     // Kortfristet gæld (CurrentLiabilities)
-    if (preg_match('/<[a-z]+:CurrentLiabilities[^>]*>(\d+)/i', $xml, $match)) {
+    if (preg_match('/<[a-z]+:CurrentLiabilities[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['kortfristetGaeld'] = (int)$match[1];
-    } elseif (preg_match('/<[a-z]+:TotalCurrentLiabilities[^>]*>(\d+)/i', $xml, $match)) {
+    } elseif (preg_match('/<[a-z]+:TotalCurrentLiabilities[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['kortfristetGaeld'] = (int)$match[1];
-    } elseif (preg_match('/CurrentLiabilities[^>]*>(\d+)/i', $xml, $match)) {
+    } elseif (preg_match('/CurrentLiabilities[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['kortfristetGaeld'] = (int)$match[1];
     }
-    // Ekstra forsøg: Nogle gange hedder det "Short term debt" eller lignende
-    elseif (preg_match('/Short[ -]term\s+(?:debt|liabilities)[^>]*>(\d+)/i', $xml, $match)) {
+    elseif (preg_match('/Short[ -]term\s+(?:debt|liabilities)[^>]*>(\d+)/i', $content, $match)) {
         $nogletal['kortfristetGaeld'] = (int)$match[1];
     }
 

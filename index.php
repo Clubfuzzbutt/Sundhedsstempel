87c00bf907d91eb64575272692c33386f24053ed
Charlotte <s214408@dtu.dk>
2026-05-29 13:35:23 +0200

    added the url for the prototype to index.php

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 //http://localhost/sundhedsstempel/demo.html
 //søg xampp contorl panel. kør apache for at få localhost til at virke
 local disk D drev. Find mappen Apache projekt. find mappen htdocs. det er mappen sundhedsstempel.
+
+//Hjemmeside infinifree til at se prototypen
+http://sundhedsstempel.infinityfree.me/index.html
 ?>
 
 
